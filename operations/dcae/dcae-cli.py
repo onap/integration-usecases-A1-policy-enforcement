2d26d2ac6ffcffea840e1b756659c4b75d9289e9
k.kedron <k.kedron@partner.samsung.com>
2021-08-19 10:51:05 +0200

    Update licence
    To fix licence issues
    
    Issue-ID: INT-1936
    Signed-off-by: Krystian Kedron <k.kedron@partner.samsung.com>
    Change-Id: I4c927d9a6b4ca8dbeb377604b9a69f667e031116

--- operations/dcae/dcae-cli.py
+++ operations/dcae/dcae-cli.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+# Copyright (C) 2021 by Samsung Electronics Co., Ltd.
 #
-# Copyright (C) 2020 by Samsung Electronics Co., Ltd.
-#
-# This software is the confidential and proprietary information of Samsung Electronics co., Ltd.
-# ("Confidential Information"). You shall not disclose such Confidential Information and shall use
-# it only in accordance with the terms of the license agreement you entered into with Samsung.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
 
 """Cli application for ONAP DCAE Dashboard for managing DCAE Microservices.
 
